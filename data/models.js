7dedb1380f65528a4776c36e2c09726a0dc44fca
podcity <podcity@gmail.com>
2025-08-05 22:54:17 +0300

    Create models.js

---
+++ data/models.js
@@ -0,0 +1,99 @@
+const cheryModels = [
+    {
+        id: 'tiggo4pro',
+        name: 'Chery Tiggo 4 Pro',
+        price: 1260000,
+        type: 'Компактный SUV',
+        engine: '1.5T, 147 л.с.',
+        drive: 'Передний привод',
+        fuel: '6.9 л/100км',
+        seats: 5,
+        highlight: 'Идеальный для города, экономичный и маневренный',
+        features: [
+            'Мультимедиа с 10.25" экраном',
+            'Камера заднего вида',
+            'Датчики парковки',
+            'Климат-контроль',
+            'Подогрев сидений'
+        ],
+        image: 'https://autospot.ru/brands/chery/tiggo4pro/img1.jpg'
+    },
+    {
+        id: 'tiggo7promax',
+        name: 'Chery Tiggo 7 Pro Max',
+        price: 1999000,
+        type: 'Среднеразмерный SUV',
+        engine: '1.6T, 186 л.с.',
+        drive: 'Полный привод',
+        fuel: '7.5 л/100км',
+        seats: 5,
+        highlight: 'Баланс комфорта и динамики, полный привод',
+        features: [
+            'Панорамная крыша',
+            'Виртуальная приборная панель',
+            'Адаптивный круиз-контроль',
+            'Система кругового обзора 360°',
+            'Беспроводная зарядка'
+        ],
+        image: 'https://autospot.ru/brands/chery/tiggo7promax/img1.jpg'
+    },
+    {
+        id: 'tiggo8promax',
+        name: 'Chery Tiggo 8 Pro Max',
+        price: 2699000,
+        type: '7-местный SUV',
+        engine: '2.0T, 249 л.с.',
+        drive: 'Полный привод',
+        fuel: '8.5 л/100км',
+        seats: 7,
+        highlight: 'Просторный 7-местный салон для большой семьи',
+        features: [
+            '3 ряда сидений',
+            'Электропривод багажника',
+            'Премиальная аудиосистема Sony',
+            'Вентиляция передних сидений',
+            'Система помощи при парковке'
+        ],
+        image: 'https://autospot.ru/brands/chery/tiggo8promax/img1.jpg'
+    },
+    {
+        id: 'arrizo8',
+        name: 'Chery Arrizo 8',
+        price: 1810000,
+        type: 'Премиальный седан',
+        engine: '1.6T, 186 л.с.',
+        drive: 'Передний привод',
+        fuel: '6.8 л/100км',
+        seats: 5,
+        highlight: 'Элегантный седан бизнес-класса с богатым оснащением',
+        features: [
+            'Премиальная отделка салона',
+            'Двойной 12.3" экран',
+            'Система шумоподавления',
+            'Адаптивная подвеска',
+            'Массаж водительского сиденья'
+        ],
+        image: 'https://autospot.ru/brands/chery/arrizo8/img1.jpg'
+    },
+    {
+        id: 'tiggo9',
+        name: 'Chery Tiggo 9',
+        price: 4150000,
+        type: 'Флагманский SUV',
+        engine: '2.0T, 261 л.с.',
+        drive: 'Полный привод AWD',
+        fuel: '9.0 л/100км',
+        seats: 7,
+        highlight: 'Флагман с максимальным уровнем роскоши и технологий',
+        features: [
+            'Пневмоподвеска CDC',
+            'Кресла Nappa кожа',
+            'HUD проекционный дисплей',
+            'Автопилот уровня L2+',
+            'Аудиосистема 14 динамиков'
+        ],
+        image: 'https://autospot.ru/brands/chery/tiggo9/img1.jpg'
+    }
+];
+
+module.exports = { cheryModels };
